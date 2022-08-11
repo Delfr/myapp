83e29cab5b620fc99716e589001a925527ffd43e
Delfr <110912698+Delfr@users.noreply.github.com>
2022-08-11 11:25:20 -0300

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,15 @@
+
+
+ <?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "usuario";
+
+
+    $conn= mysqli_connect($servername, $username, "", $dbname);
+
+
+
+ ?>
