f23f1a830b5b486c6c9f703ab1a1349c49b9a33d
Delfr <110912698+Delfr@users.noreply.github.com>
2022-08-16 17:34:54 -0300

    Update formprueba.php

--- formprueba.php
+++ formprueba.php
@@ -14,10 +14,6 @@ if(!isset($_POST['nombre'])||!isset($_POST['password'])||!isset($_POST['email'])
   $email = validar_email($_POST['email']);
   $curso = mysqli_real_escape_string($conectar_db,$_POST['curso']);
 
-  echo "Gracias $nombre, te enviaremos un email a $email";
-
-
-
  function ingresar_usuario($conectar_db, $nombre, $password, $email, $curso){
   $sql_insert= "INSERT INTO tpractica(nombre, password, email, curso) VALUES ('$nombre', '$password' '$email', '$curso')";
   $data_entry = mysqli_query($conectar_db, $sql_insert);
