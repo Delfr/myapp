041064dab9d15ce5b598e341e083e20f134b74f5
Delfr <110912698+Delfr@users.noreply.github.com>
2022-08-17 15:03:58 -0300

    Add files via upload

--- formprueba.php
+++ formprueba.php
@@ -1,36 +1,33 @@
+
 <?php
 
 require("connect.php");
 
+function ingresar_usuario($conectar_db, $nombre, $password, $email, $curso){
+ $sql_insert= "INSERT INTO tpractica(nombre, password, email, curso) VALUES ('$nombre','$password','$email', '$curso')";
+ $data_entry = mysqli_query($conectar_db, $sql_insert);
+
+   if($data_entry){
+   echo "Los datos fueron registrados";
+   } else {
+     die(mysqli_connect_errno());
+     echo "Los datos no fueron registrados";
+   }
+ }
+
+ function validar_email(string $email){
+   return filter_var($email, FILTER_VALIDATE_EMAIL);}
 
 if(!isset($_POST['nombre'])||!isset($_POST['password'])||!isset($_POST['email'])||!isset($_POST['curso'])){
   echo "Por favor complete todos los campos";
   die();
+  } else {
+    $nombre = mysqli_real_escape_string($conectar_db, $_POST['nombre']);
+    $password = mysqli_real_escape_string($conectar_db,$_POST['password']);
+    $email = validar_email($_POST['email']);
+    $curso = mysqli_real_escape_string($conectar_db,$_POST['curso']);
 
-} else {
-
-  $nombre = mysqli_real_escape_string($conectar_db, $_POST['nombre']);
-  $password = mysqli_real_escape_string($conectar_db,$_POST['password']);
-  $email = validar_email($_POST['email']);
-  $curso = mysqli_real_escape_string($conectar_db,$_POST['curso']);
-
- function ingresar_usuario($conectar_db, $nombre, $password, $email, $curso){
-  $sql_insert= "INSERT INTO tpractica(nombre, password, email, curso) VALUES ('$nombre', '$password' '$email', '$curso')";
-  $data_entry = mysqli_query($conectar_db, $sql_insert);
-  return $data_entry;
-
-  if($data_entry){
-    echo "Los datos fueron registrados";
-    } else {
-      die(mysqli_connect_errno());
-      echo "Los datos no fueron registrados";
-    }
+    ingresar_usuario($conectar_db, $nombre, $password, $email, $curso);
   }
-}
 
-function validar_email(string $email){
-  return filter_var($email, FILTER_VALIDATE_EMAIL);
-}
-
-ingresar_usuario($conectar_db, $nombre, $password, $email, $curso);
 mysqli_close($conectar_db);
